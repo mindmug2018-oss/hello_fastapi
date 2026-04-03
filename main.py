b37e31d89654dcd0bcca2c7d506549f470f14a4a
Shinichiro <mindmug2018@gmail.com>
2026-04-03 17:57:14 +0900

    fifth commit end of day

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, Session
+
+
+
+# Calling Database
+DB_URL = "postgresql://scott:tiger@localhost/scott_db"
+engine = create_engine(DB_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 app=FastAPI()
@@ -10,10 +27,22 @@ templates = Jinja2Templates(directory='templates')
 
 @app.get('/', response_class=HTMLResponse)
 
-def home(request:Request):
-    result = templates.TemplateResponse('index.html', {
-    'request':request,
-    'fortuneToday': 'You will meet your fated partner today!'
-    })    
-    return result
+def home(request:Request, db: Session = Depends(get_db)):
+    query = text('''
+        SELECT num, content
+        FROM notice
+        ORDER BY num DESC
+    ''')
+    result = db.execute(query)
+    notice_list = result.fetchall()
+    
+    result2 = templates.TemplateResponse(
+    {'request':request},
+    name='index.html',
+    context={
+        'fortuneToday': 'You will meet your fated partner today!',
+        "noticelist":notice_list
+        }
+    )    
+    return result2
 
